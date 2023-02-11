b542d05c097c4d76e585ec5860e8b1896ff4879c
TheUserOne <theuserone@yandex.ru>
2023-02-11 10:35:35 +0700

    Example005_ConditionIfElse - DONE

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please Enter Your Name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "the one")
+{
+    Console.WriteLine("Happy to see you The One");
+} 
+else
+{
+    Console.Write ("Hello ");
+    Console.WriteLine (username);
+}
