4a68f0cfb403c41acb2f343641fc653db31fac58
TheUserOne <theuserone@yandex.ru>
2023-02-11 12:41:51 +0700

    Example006_Base - DONE

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = 4;
+int numberB = 5;
+int numberC = 9;
+int numberD = 14;
+int numberE = 2;
+
+int numberMAX = numberA;
+
+if (numberA > numberMAX) numberMAX = numberA;
+if (numberB > numberMAX) numberMAX = numberB;
+if (numberC > numberMAX) numberMAX = numberC;
+if (numberD > numberMAX) numberMAX = numberD;
+if (numberE > numberMAX) numberMAX = numberE;
+
+Console.WriteLine (numberMAX);
